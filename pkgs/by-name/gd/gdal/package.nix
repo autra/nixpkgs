f4d9c126a5c2014c85e2f29e2a1c11b2e4293c0d
Augustin Trancart <augustin.trancart@gmail.com>
2025-11-08 00:21:04 +0100

    gdal: 3.11.4 -> 3.12

--- pkgs/by-name/gd/gdal/package.nix
+++ pkgs/by-name/gd/gdal/package.nix
@@ -84,13 +84,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "gdal" + lib.optionalString useMinimalFeatures "-minimal";
-  version = "3.11.4";
+  version = "3.12.0";
 
   src = fetchFromGitHub {
     owner = "OSGeo";
     repo = "gdal";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-CFQF3vDhhXsAnIfUcn6oTQ4Xm+GH/36dqSGc0HvyEJ0=";
+    hash = "sha256-al7nDmUbKNP4kBMVBLWUUxn0QSAbYAJ1XhZWAVFk+sA=";
   };
 
   patches = [
